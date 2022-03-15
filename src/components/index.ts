26b542b2a1004a538d0c467e5c16b96601740d0d
唐皓晨 <2290774104@qq.com>
2022-03-15 11:12:07 +0800

    init

---
+++ src/components/index.ts
@@ -0,0 +1,20 @@
+import _Vue, { PluginFunction, VueConstructor } from 'vue'
+
+interface InstallFunction extends PluginFunction<any> {
+  installed?: boolean
+}
+
+const Components: { [key: string]: VueConstructor } = {
+}
+
+const install: InstallFunction = (Vue: typeof _Vue) => {
+  if (install.installed) return
+
+  Object.keys(Components).forEach((name: any) => {
+    Vue.component(name, Components[name])
+  })
+
+  install.installed = true
+}
+
+export default install
